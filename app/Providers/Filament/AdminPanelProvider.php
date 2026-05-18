4116242fac1f32b756621ae751079bef6c8a5696
John Doe <vaska.i.88@gmail.com>
2026-05-18 23:27:25 +0300

    refactor: remove unused widgets from AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,8 +51,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
+                //AccountWidget::class,
+                //FilamentInfoWidget::class,
                 \App\Filament\Widgets\StatsOverview::class,
             ])
             ->middleware([
