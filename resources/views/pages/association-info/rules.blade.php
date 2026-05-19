62266e3530846b6066af32f552c9268ea53cd2d9
John Doe <vaska.i.88@gmail.com>
2026-05-19 23:52:57 +0300

    style: enhance layout and typography for rules section to improve readability and responsiveness

--- resources/views/pages/association-info/rules.blade.php
+++ resources/views/pages/association-info/rules.blade.php
@@ -8,17 +8,17 @@ bgImage="{{ asset('images/bg/rules.png') }}"
     
 </x-hero-section>
 {{-- ===== Кто может вступить ===== --}}
-  <section class="py-10 bg-white">
-    <div class="max-w-(--breakpoint-2xl) mx-auto bg-[#F8F8F8] flex flex-col md:flex-row gap-10 items-center">
+  <section class="md:py-10 py-6 bg-white">
+    <div class="max-w-(--breakpoint-2xl) mx-auto bg-[#F8F8F8] flex flex-col md:flex-row gap-10 items-center px-4 md:px-2">
         <div class="info px-4 sm:px-6 lg:px-8">
-            <h2 class="section-title a-font text-[#2E325C] text-5xl mb-8">Кто может стать членом Ассоциации</h2>
-            <p class="text-brand-gray font-medium text-lg mb-4">Членами Ассоциации могут быть:</p>
-            <ul class="list-disc text-brand-gray pl-4 text-lg space-y-4 mb-8">
+            <h2 class="section-title a-font text-[#2E325C] text-5xl mb-4 md:mb-8">Кто может стать членом Ассоциации</h2>
+            <p class="text-brand-gray font-medium text-lg mb-2 md:mb-4">Членами Ассоциации могут быть:</p>
+            <ul class="list-disc text-brand-gray pl-4 text-lg space-y-4 mb-2 md:mb-8">
                 <li>владельцы яхт класса Carter 30</li>
                 <li>участники экипажей</li>
                 <li>лица, заинтересованные в развитии класса</li>
             </ul>
-            <button class="mt-6 bg-[#2D92CE] text-white py-2 px-6 hover:bg-[#0074CC] transition-colors text-lg font-semibold">
+            <button class="mt-6 bg-[#2D92CE] text-white py-2 px-6 hover:bg-[#0074CC] w-full md:max-w-[300px] transition-colors text-lg font-semibold">
             Подать заявку →
             </button>
         </div>
@@ -27,93 +27,93 @@ bgImage="{{ asset('images/bg/rules.png') }}"
         </div>
     </div>
   </section>
-  <section class="max-w-(--breakpoint-2xl) mx-auto mb-24">
+  <section class="max-w-(--breakpoint-2xl) mx-auto md:mb-24 mb-8 px-4 md:px-2">
     <h2 class="section-title a-font text-[#2E325C] text-5xl mb-8">Условия вступления</h2>
-    <div class="flex gap-4">
-        <div class="card bg-[#F8F8F8] p-6 text-center">
-            <img src="{{ asset('images/rules/rules_1.svg') }}" class="m-auto mb-4" alt="">
-            <p class="text-[#2E325C] text-lg">Соблюдение устава и регламентов</p>
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div class="card bg-[#F8F8F8] md:p-6 p-3 text-center">
+            <img src="{{ asset('images/rules/rules_1.svg') }}" class="m-auto max-w-12 md:max-w-full mb-4" alt="">
+            <p class="text-[#2E325C] md:text-lg text-sm">Соблюдение устава и регламентов</p>
         </div>
-        <div class="card bg-[#F8F8F8] p-6 text-center">
-            <img src="{{ asset('images/rules/rules_2.svg') }}" class="m-auto mb-4" alt="">
-            <p class="text-[#2E325C] text-lg">Участие в деятельности Ассоциации</p>
+        <div class="card bg-[#F8F8F8] md:p-6 p-3 text-center">
+            <img src="{{ asset('images/rules/rules_2.svg') }}" class="m-auto max-w-12 md:max-w-full mb-4" alt="">
+            <p class="text-[#2E325C] md:text-lg text-sm">Участие в деятельности Ассоциации</p>
         </div>
-        <div class="card bg-[#F8F8F8] p-6 text-center">
-            <img src="{{ asset('images/rules/rules_3.svg') }}" class="m-auto mb-4" alt="">
-            <p class="text-[#2E325C] text-lg">Предоставление необходимых данных</p>
+        <div class="card bg-[#F8F8F8] md:p-6 p-3 text-center">
+            <img src="{{ asset('images/rules/rules_3.svg') }}" class="m-auto max-w-12 md:max-w-full mb-4" alt="">
+            <p class="text-[#2E325C] md:text-lg text-sm">Предоставление необходимых данных</p>
         </div>
-        <div class="card bg-[#F8F8F8] p-6 text-center">
-            <img src="{{ asset('images/rules/rules_4.svg') }}" class="m-auto mb-4" alt="">
-            <p class="text-[#2E325C] text-lg">Подтверждение заявки</p>
+        <div class="card bg-[#F8F8F8] md:p-6 p-3 text-center">
+            <img src="{{ asset('images/rules/rules_4.svg') }}" class="m-auto max-w-12 md:max-w-full mb-4" alt="">
+            <p class="text-[#2E325C] md:text-lg text-sm">Подтверждение заявки</p>
         </div>
     </div>
   </section>
-  <section class="max-w-(--breakpoint-2xl) mx-auto mb-24">
+  <section class="max-w-(--breakpoint-2xl) mx-auto md:mb-24 mb-8 px-4 md:px-2">
   <h2 class="section-title a-font text-[#2E325C] text-5xl mb-8">Порядок вступления</h2>
  
     <!-- Steps row -->
-    <div class="flex items-start">
+    <div class="md:flex items-start flex-col md:flex-row">
  
       <!-- Left stub line -->
-      <div class="h-px w-32 bg-[#F24842] mt-12 shrink-0"></div>
+      <div class="h-px w-32 hidden md:block bg-[#F24842] mt-12 shrink-0"></div>
  
       <!-- Step 1 -->
-      <div class="flex flex-col items-center flex-1">
-        <div class="w-24 h-24 rounded-full border border-[#F24842] flex items-center justify-center">
-          <span class="text-[#F24842] font-medium text-5xl leading-none">1</span>
+      <div class="flex md:flex-col items-center flex-1 gap-x-2">
+        <div class="md:size-24 size-16 rounded-full border border-[#F24842] flex items-center justify-center">
+          <span class="text-[#F24842] font-medium text-5xl leading-none a-font">1</span>
         </div>
-        <p class="mt-4 text-center text-lg text-[#1e2a47] leading-snug">
+        <p class="md:mt-4 md:text-center md:text-lg text-[#1e2a47] leading-snug">
           Подача заявки<br/>через сайт
         </p>
       </div>
  
       <!-- Connector -->
-      <div class="h-px flex-1 bg-[#F24842] mt-12 min-w-[24px]"></div>
+      <div class="flex-1 bg-[#F24842] md:mt-12 md:h-px md:min-w-[24px] h-8 w-px ml-8 md:ml-0"></div>
  
       <!-- Step 2 -->
-      <div class="flex flex-col items-center flex-1">
-        <div class="w-24 h-24 rounded-full border border-[#F24842] flex items-center justify-center">
-          <span class="text-[#F24842] font-medium text-5xl leading-none">2</span>
+      <div class="flex md:flex-col items-center flex-1 gap-x-2">
+        <div class="md:size-24 size-16 rounded-full border border-[#F24842] flex items-center justify-center">
+          <span class="text-[#F24842] font-medium text-5xl leading-none a-font">2</span>
         </div>
-        <p class="mt-4 text-center text-lg text-[#1e2a47] leading-snug">
+        <p class="md:mt-4 md:text-center md:text-lg text-[#1e2a47] leading-snug">
           Проверка данных
         </p>
       </div>
  
       <!-- Connector -->
-      <div class="h-px flex-1 bg-[#F24842] mt-12 min-w-[24px]"></div>
+      <div class="flex-1 bg-[#F24842] md:mt-12 md:h-px md:min-w-[24px] h-8 w-px ml-8 md:ml-0"></div>
  
       <!-- Step 3 -->
-      <div class="flex flex-col items-center flex-1">
-        <div class="w-24 h-24 rounded-full border border-[#F24842] flex items-center justify-center">
-          <span class="text-[#F24842] font-medium text-5xl leading-none">3</span>
+      <div class="flex md:flex-col items-center flex-1 gap-x-2">
+        <div class="md:size-24 size-16 rounded-full border border-[#F24842] flex items-center justify-center">
+          <span class="text-[#F24842] font-medium text-5xl leading-none a-font">3</span>
         </div>
-        <p class="mt-4 text-center text-lg text-[#1e2a47] leading-snug">
+        <p class="md:mt-4 md:text-center md:text-lg text-[#1e2a47] leading-snug">
           Подтверждение<br/>участия
         </p>
       </div>
  
       <!-- Connector -->
-      <div class="h-px flex-1 bg-[#F24842] mt-12 min-w-[24px]"></div>
+      <div class="flex-1 bg-[#F24842] md:mt-12 md:h-px md:min-w-[24px] h-8 w-px ml-8 md:ml-0"></div>
  
       <!-- Step 4 -->
-      <div class="flex flex-col items-center flex-1">
-        <div class="w-24 h-24 rounded-full border border-[#F24842] flex items-center justify-center">
-          <span class="text-[#F24842] font-medium text-5xl leading-none">4</span>
+      <div class="flex md:flex-col items-center flex-1 gap-x-2">
+        <div class="md:size-24 size-16 rounded-full border border-[#F24842] flex items-center justify-center">
+          <span class="text-[#F24842] font-medium text-5xl leading-none a-font">4</span>
         </div>
-        <p class="mt-4 text-center text-lg text-[#1e2a47] leading-snug">
+        <p class="md:mt-4 md:text-center md:text-lg text-[#1e2a47] leading-snug">
           Включение<br/>в состав Ассоциации
         </p>
       </div>
  
       <!-- Right stub line -->
-      <div class="h-px w-32 bg-[#F24842] mt-12 shrink-0"></div>
+      <div class="h-px w-32 hidden md:block bg-[#F24842] mt-12 shrink-0"></div>
  
     </div>
   </section>
-  <section style="background-image: url('{{ asset('images/rules/rules_want.png') }}')" class="max-w-(--breakpoint-2xl) mx-auto bg-cover bg-center py-20 mt-10 flex flex-col items-center text-center mb-8">
-    <h2 class="a-font text-white text-5xl max-w-4xl">Хотите присоединиться к Ассоциации и принимать участие в её деятельности?</h2>
-    <button class="mt-6 bg-white text-[#2E325C] py-2 px-9 hover:bg-[#E8E8E8] transition-colors text-lg font-semibold">
+  <section style="background-image: url('{{ asset('images/rules/rules_want.png') }}')" class="max-w-(--breakpoint-2xl) mx-auto bg-cover bg-center py-20 px-3 md:px-0 mt-10 flex flex-col items-center text-center mb-8">
+    <h2 class="a-font text-white text-3xl md:text-5xl max-w-4xl">Хотите присоединиться к Ассоциации и принимать участие в её деятельности?</h2>
+    <button class="mt-6 bg-white text-[#2E325C] py-2 px-9 hover:bg-[#E8E8E8] transition-colors text-lg font-semibold w-full ms">
         Подать заявку  →
     </button>
   </section>
