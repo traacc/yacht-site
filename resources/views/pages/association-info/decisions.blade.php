5c91a8834c476076f26971441774c6c87e67d4f8
John Doe <vaska.i.88@gmail.com>
2026-05-20 00:21:59 +0300

    style: improve layout and typography for documents section to enhance readability and responsiveness

--- resources/views/pages/association-info/decisions.blade.php
+++ resources/views/pages/association-info/decisions.blade.php
@@ -23,16 +23,16 @@ bgImage="{{ asset('images/bg/decisions.png') }}"
             </div>
         </div>
         
-        <div class="grid grid-cols-2 md:grid-cols-3 gap-6">
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             <template x-for="doc in currentDocs" :key="doc.id">
                 <div class="bg-[#F8F8F8] flex gap-4 hover:shadow-md transition-shadow cursor-pointer p-4">
-                    <div class="max-w-16">
+                    <div class="max-w-10 md:max-w-16">
                         <img class="w-full" src="{{ asset('images/icons/pdf.png') }}" alt="">
                     </div>
                     <div class="">
-                        <div class="text-[#2E325C] text-lg font-semibold mb-4" x-text='doc.title'></div>
-                        <div class="text-brand-gray-light font-medium mb-4" x-text='doc.desc'></div>
-                        <a x-bind:href="doc.path" class="text-[#2E325C] text-lg font-semibold flex gap-4 items-center"><img src="{{ asset('images/icons/download.svg') }}" alt=""> <span>Скачать PDF</span></a>
+                        <div class="text-[#2E325C] text-sm md:text-lg font-semibold mb-4" x-text='doc.title'></div>
+                        <div class="text-brand-gray-light font-medium mb-4 text-xs md:text-base" x-text='doc.desc'></div>
+                        <a x-bind:href="doc.path" class="text-[#2E325C] text-sm md:text-lg font-semibold flex gap-4 items-center"><img src="{{ asset('images/icons/download.svg') }}" alt=""> <span>Скачать PDF</span></a>
                     </div>
                 </div>
             </template>
@@ -179,12 +179,12 @@ bgImage="{{ asset('images/bg/decisions.png') }}"
     }
 </script>
 
-<section class="py-10 bg-white">
+<section class="py-10 bg-white px-4 sm:px-6 lg:px-8">
 <div class="max-w-(--breakpoint-2xl) mx-auto bg-[#F8F8F8] flex flex-col md:flex-row gap-10 items-center">
     <div class="info px-4 sm:px-6 lg:px-8">
-        <h2 class="section-title a-font text-[#2E325C] text-5xl mb-8">О решениях общего собрания</h2>
-        <p class="text-brand-gray font-medium text-lg mb-4">Решения общего собрания принимаются участниками Ассоциации и регулируют ключевые вопросы её деятельности, включая проведение соревнований, изменения в регламенте и развитие класса Carter 30.</p>
-        <p class="text-brand-gray font-medium text-lg">Все решения фиксируются в протоколах и являются обязательными для исполнения участниками Ассоциации.</p>
+        <h2 class="section-title a-font text-[#2E325C] text-2xl md:text-5xl mb-8">О решениях общего собрания</h2>
+        <p class="text-brand-gray font-medium text-sm md:text-lg mb-4">Решения общего собрания принимаются участниками Ассоциации и регулируют ключевые вопросы её деятельности, включая проведение соревнований, изменения в регламенте и развитие класса Carter 30.</p>
+        <p class="text-brand-gray font-medium text-sm md:text-lg">Все решения фиксируются в протоколах и являются обязательными для исполнения участниками Ассоциации.</p>
     </div>
     <div class="pic max-w-[720px] shrink-0">
         <img class="w-full" src="{{ asset('images/decisions.png') }}" alt="">
