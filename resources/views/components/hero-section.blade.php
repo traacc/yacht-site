2f3e98c1a0a1a1794284d4e7e3a793891b8bb083
John Doe <vaska.i.88@gmail.com>
2026-05-19 19:07:15 +0300

    style: update hero section layout and typography for improved readability

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -1,12 +1,12 @@
 @props(['title' => '', 'desc' => '', 'bgImage' => ''])
-<section style="background-image: url({{ $bgImage }})" class="feedback-form bg-cover relative 
+<section style="background-image: url({{ $bgImage }})" class="bg-cover relative px-4 md:px-2
             after:absolute after:inset-0 
             after:content-[''] 
             after:bg-linear-to-t 
             after:from-[#2E325C] 
             after:to-transparent">
     <div class="max-w-(--breakpoint-2xl) m-auto text-white pt-80 relative z-40 pb-8">
-        <h2 class="a-font text-6xl">{{ $title }}</h2>
-        <p class="max-w-[768px]">{{ $desc }}</p>
+        <h2 class="a-font text-2xl md:text-6xl">{{ $title }}</h2>
+        <p class="max-w-[768px] text-sm md:text-base">{{ $desc }}</p>
     </div>
 </section>
