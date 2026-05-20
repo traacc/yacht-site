a8cb8355f4bef731198e9027dd9110c35dcddd9a
John Doe <vaska.i.88@gmail.com>
2026-05-20 09:56:59 +0300

    style: add RaceResultSeeder for seeding race result data

---
+++ database/seeders/RaceResultSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\RaceResult;
+use Illuminate\Database\Seeder;
+
+class RaceResultSeeder extends Seeder
+{
+    public function run(): void
+    {
+        RaceResult::factory()->count(50)->create();
+    }
+}
