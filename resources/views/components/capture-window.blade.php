5007a9d04761f83dad92eaae278f889c6adf5b26
John Doe <vaska.i.88@gmail.com>
2026-05-21 12:25:50 +0300

    fix: capture window social

--- resources/views/components/capture-window.blade.php
+++ resources/views/components/capture-window.blade.php
@@ -82,7 +82,7 @@
                             width: 64px;
                         }
                     </style>
-                    <div class="social mt-2 flex gap-2">
+                    <div class="social mt-2 flex gap-2 justify-center">
                         <a href="#" class="text-[#2D92CE] hover:text-white">
                             {!! file_get_contents(public_path('images/social_icons/tl.svg')) !!}
                         </a>
