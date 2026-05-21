14f64b7bc4738e487e176a94a952ec71aa3f0b12
John Doe <vaska.i.88@gmail.com>
2026-05-21 10:54:21 +0300

    fix: optimize

--- app/Filament/User/Resources/RegattaEntries/Pages/ManageRegattaEntries.php
+++ app/Filament/User/Resources/RegattaEntries/Pages/ManageRegattaEntries.php
@@ -13,7 +13,7 @@ class ManageRegattaEntries extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            //CreateAction::make(),
         ];
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Filament\Notifications\Notification;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Notification::configureUsing(function (Notification $notification): void {
+            $notification->duration(2000); // 2000 мс = 2 секунды
+        });
     }
 }
