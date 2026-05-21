78a2901e9923cc769f92601d2fb9a39f217aac2a
John Doe <vaska.i.88@gmail.com>
2026-05-21 22:57:41 +0300

    fix: hero section height

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -5,7 +5,7 @@
             after:bg-linear-to-t 
             after:from-[#2E325C] 
             after:to-transparent">
-    <div class="container m-auto text-white pt-80 relative z-40 pb-8">
+    <div class="container m-auto text-white pt-[550px] relative z-40 pb-8">
         <h2 class="a-font text-2xl/5 mb-3 md:text-6xl">{{ $title }}</h2>
         <p class="max-w-[768px] text-sm md:text-2xl">{{ $desc }}</p>
     </div>
