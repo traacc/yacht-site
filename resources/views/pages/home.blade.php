a6be5c087f5c1ade64ce86fc4e65678fd7416040
John Doe <vaska.i.88@gmail.com>
2026-05-24 11:21:31 +0300

    fix: ratings in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -159,7 +159,7 @@
                             </tr>
                         </thead>
                         <tbody class="divide-y text-center font-medium">
-                            <template x-data="{ teams: [['Барс', 7.0], ['Барс', 7.0], ['Барс', 7.0], ['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0]] }" x-for="(team, i) in teams" :key="i">
+                            <template x-data="{ teams: [['Барс', 7.0], ['Барс', 7.0], ['Барс', 7.0]] }" x-for="(team, i) in teams" :key="i">
                                 <tr>
                                     <td class="py-2" data-label="Место">
                                         <div class="flex items-center md:justify-center gap-3">
@@ -188,7 +188,7 @@
                             </tr>
                         </thead>
                         <tbody class="divide-y text-center font-medium">
-                            <template x-data="{ participants: [['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0]] }" x-for="(p, i) in participants" :key="i">
+                            <template x-data="{ participants: [['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0], ['Игорь Скалин', 7.0]] }" x-for="(p, i) in participants" :key="i">
                                 <tr>
                                     <td class="py-2" data-label="Место">
                                         <div class="flex items-center md:justify-center gap-3">
