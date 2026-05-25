3cb1246a02afa502d763185cd45d891001080e59
John Doe <vaska.i.88@gmail.com>
2026-05-25 16:58:30 +0300

    fix: return and fixed top closest block

--- resources/views/livewire/home-closest-regatta.blade.php
+++ resources/views/livewire/home-closest-regatta.blade.php
@@ -6,9 +6,8 @@
 
     @if($regatta)
     <div class="container mx-auto relative mt-4">
-        <!--
         {{-- Карточка ближайшей регаты --}}
-        <div class="md:absolute md:top-8 md:left-8 mx-4 md:mx-0 mt-6 bg-[#00000080] backdrop-blur-xs p-3 md:p-4  md:w-auto md:max-w-2xl shadow-2xl">
+        <div class="md:absolute md:top-16 md:left-8 mx-4 md:mx-0 mt-6 bg-[#00000080] backdrop-blur-xs p-3 md:p-4  md:w-auto md:max-w-2xl shadow-2xl">
             <div class="flex items-center gap-2 mb-1 md:mb-3">
                 <span class="rounded-full bg-[#F24842] text-center text-white flex justify-center items-center shrink-0 aspect-square size-6 p-1 md:p-0 md:size-11">
                     {!!  file_get_contents(public_path('images/icons/calendar.svg')) !!}
@@ -41,7 +40,6 @@
 
 
         </div>
-        -->
     </div>
     @endif
 </section>
