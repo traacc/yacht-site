b020bb8e52841333c5e348066a6b597d2ca1c63e
John Doe <vaska.i.88@gmail.com>
2026-05-25 16:26:24 +0300

    style: text in ol

--- resources/views/pages/association-info/policy.blade.php
+++ resources/views/pages/association-info/policy.blade.php
@@ -12,29 +12,15 @@ bgImage="{{ asset('images/bg/policy.png') }}"
     <div class="container mx-auto">
         <h2 class="section-title a-font text-[#2E325C] mb-4 md:mb-8">Политика Ассоциации</h2>
 
-        <div class="text-brand-gray font-medium">
-            <div class="mb-4 md:mb-8">
-                <p class="md:text-base text-sm">Ассоциация открыта для любых владельцев яхт проекта Carter 30</p>
-            </div>
-            <div class="mb-4 md:mb-8">
-                <p class="md:text-base text-sm">Ассоциация – не бизнес ни для кого. Все доходы Ассоциации идут только на её развитие и поддержку её членов</p>
-            </div>
-            <div class="mb-4 md:mb-8">
-                <p class="md:text-base text-sm">Правила класса – чётко описанные Box Rules. То, что явно не запрещено правилами класса – разрешено</p>
-            </div>
-            <div class="mb-4 md:mb-8">
-                <p class="md:text-base text-sm">Любой член Ассоциации может вынести любой вопрос на общее обсуждение</p>
-            </div>
-            <div class="mb-4 md:mb-8">
-                <p class="md:text-base text-sm">Активное привлечение молодёжи</p>
-            </div>
-            <div class="mb-4 md:mb-8">
-                <p class="md:text-base text-sm">Организация соревнований высокой конкуренции</p>
-            </div>
-            <div class="mb-4 md:mb-8">
-                <p class="md:text-base text-sm">Материально-техническая и консультационная поддержка членов Ассоциации</p>
-            </div>
-        </div>
+        <ol class="text-brand-gray font-medium list-decimal list-inside space-y-4 md:text-base text-sm">
+            <li>Ассоциация открыта для любых владельцев яхт проекта Carter 30</li>
+            <li>Ассоциация – не бизнес ни для кого. Все доходы Ассоциации идут только на её развитие и поддержку её членов</li>
+            <li>Правила класса – чётко описанные Box Rules. То, что явно не запрещено правилами класса – разрешено</li>
+            <li>Любой член Ассоциации может вынести любой вопрос на общее обсуждение</li>
+            <li>Активное привлечение молодёжи</li>
+            <li>Организация соревнований высокой конкуренции</li>
+            <li>Материально-техническая и консультационная поддержка членов Ассоциации</li>
+        </ol>
     </div>
     
 
