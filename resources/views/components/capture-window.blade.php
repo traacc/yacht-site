a36b0ff1ce048f6079316cc5d35efa6d5df2770d
John Doe <vaska.i.88@gmail.com>
2026-05-27 00:37:21 +0300

    fix: window

--- resources/views/components/capture-window.blade.php
+++ resources/views/components/capture-window.blade.php
@@ -1,17 +1,14 @@
+@guest
 <div x-data="{
         isOpen: false,
-        getCookie(name) {
-            const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-            return match ? decodeURIComponent(match[2]) : null;
-        },
         init() {
-            if (!this.getCookie('capture_window_shown')) {
-                setTimeout(() => { this.isOpen = true; }, 120000);
+            if (!localStorage.getItem('capture_window_shown')) {
+                setTimeout(() => { this.isOpen = true; }, 600000);
             }
         },
         closeModal() {
             this.isOpen = false;
-            document.cookie = 'capture_window_shown=1; path=/';
+            localStorage.setItem('capture_window_shown', '1');
         }
      }"
      x-show="isOpen"
@@ -78,3 +75,4 @@
         </div>
     </div>
 </div>
+@endguest
