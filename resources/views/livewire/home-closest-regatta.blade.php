6e8785864cd54e428a5d0ace92e90e2f1ea5037d
John Doe <vaska.i.88@gmail.com>
2026-05-27 02:03:54 +0300

    style: replace bg

--- resources/views/livewire/home-closest-regatta.blade.php
+++ resources/views/livewire/home-closest-regatta.blade.php
@@ -1,13 +1,13 @@
 <section class="relative h-[480px] md:h-[788px]">
     <!--<video autoplay muted playsinline loop src="{{ '/videos/hero_video_2.mp4' }}"  class="absolute inset-0 w-full h-full object-cover"></video>-->
-    <img class="absolute inset-0 -top-15 object-[29%_50%] lg:object-[50%_50%] lg:-top-30 w-full h-full object-cover" src="{{ asset('/images/bg/bg_hero.webp') }}" alt="">
+    <img class="absolute inset-0 object-[29%_50%] lg:object-[50%_50%] lg:top-0 w-full h-full object-cover" src="{{ asset('/images/bg/bg_hero.webp') }}" alt="">
 
     <div class="hero-overlay absolute inset-0"></div>
 
     @if($regatta)
     <div class="container mx-auto relative mt-4">
         {{-- Карточка ближайшей регаты --}}
-        <div class="md:absolute md:top-16 md:left-8 mx-4 md:mx-0 mt-6 bg-[#00000080] backdrop-blur-xs p-3 md:p-4  md:w-auto md:max-w-2xl shadow-2xl">
+        <div class="md:absolute md:top-6 md:left-8 mx-4 md:mx-0 mt-6 bg-[#00000080] backdrop-blur-xs p-3 md:p-4  md:w-auto md:max-w-2xl shadow-2xl">
             <div class="flex items-center gap-2 mb-1 md:mb-3">
                 <span class="rounded-full bg-[#F24842] text-center text-white flex justify-center items-center shrink-0 aspect-square size-6 p-1 md:p-0 md:size-11">
                     {!!  file_get_contents(public_path('images/icons/calendar.svg')) !!}
