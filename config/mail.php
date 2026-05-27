a1e96bd7126724248f45c93a461b40a70981218e
John Doe <vaska.i.88@gmail.com>
2026-05-28 00:49:10 +0300

    fix: mail something

--- config/mail.php
+++ config/mail.php
@@ -39,13 +39,13 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
+            'scheme' => env('MAIL_SCHEME') ?: null,
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'encryption' => env('MAIL_ENCRYPTION', null),
+            'username' => env('MAIL_USERNAME') ?: null,
+            'password' => env('MAIL_PASSWORD') ?: null,
+            'encryption' => env('MAIL_ENCRYPTION') ?: null,
             'timeout' => null,
             //'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
             'stream'     => [
