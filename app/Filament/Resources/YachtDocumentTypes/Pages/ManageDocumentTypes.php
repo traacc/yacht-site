f4fd3693cc11307a2cc7cccf04ebb8e9f87805ee
John Doe <vaska.i.88@gmail.com>
2026-05-28 19:35:33 +0300

    fix: type error

--- app/Filament/Resources/YachtDocumentTypes/Pages/ManageDocumentTypes.php
+++ app/Filament/Resources/YachtDocumentTypes/Pages/ManageDocumentTypes.php
@@ -16,7 +16,7 @@ class ManageDocumentTypes extends ManageRecords
     {
         return [
             CreateAction::make()->modalHeading('Новый тип документа')
-                ->label('Добавит документ')
+                ->label('Добавить документ')
                 ->createAnother(false),
         ];
     }
