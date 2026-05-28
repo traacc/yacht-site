c7c568aa05a93dfb9da9e0ef5da6d37341af4d6f
John Doe <vaska.i.88@gmail.com>
2026-05-28 19:28:30 +0300

    fix: type

--- app/Filament/Resources/Regattas/Pages/ManageRegattas.php
+++ app/Filament/Resources/Regattas/Pages/ManageRegattas.php
@@ -30,7 +30,7 @@ class ManageRegattas extends ManageRecords
     {
         return [
             Action::make('documentSettings')
-                ->label('Типы документов')
+                ->label('Документы')
                 ->icon('heroicon-o-document-text')
                 ->color('white')
                 ->url(fn () => \App\Filament\Resources\YachtDocumentTypeResource::getUrl()),
