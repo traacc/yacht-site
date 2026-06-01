5d13da0eca06293fa435ce1e662646de694ba8b9
John Doe <vaska.i.88@gmail.com>
2026-06-01 18:44:55 +0300

    fix: typo

--- resources/views/livewire/auth/login-modal.blade.php
+++ resources/views/livewire/auth/login-modal.blade.php
@@ -137,7 +137,7 @@
                         x-init="initFlatpickr()">
                             <input
                             x-ref="input" 
-                            x-model="value" placeholder="Выберите дату"
+                            x-model="value" placeholder="Дата рождения"
                             type="date" id="birthday" wire:model="birthday" placeholder="Дата рождения" max="2016-12-31" min="1926-01-01" onkeydown="return false"
                                 class="mt-1 block w-full border-0 border-b border-[#EAEAEA] shadow-xs focus:border-indigo-500 focus:ring-indigo-500 text-xs md:text-base @error('birthday') border-red-300 @enderror">
                         </div>
