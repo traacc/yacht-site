4a50ac91cf5fb74762a17546c0264af6b4916212
John Doe <vaska.i.88@gmail.com>
2026-06-03 11:22:34 +0300

    feat: restore delete user

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -197,7 +197,7 @@ class UserResource extends Resource
             ], layout: FiltersLayout::AboveContent)->filtersFormColumns(3)->deferFilters(false)
             ->recordActions([
                 EditAction::make()->modalHeading('Редактировать пользователя'),
-                //DeleteAction::make(),
+                DeleteAction::make(),
                 ForceDeleteAction::make(),
                 RestoreAction::make(),
             ])
