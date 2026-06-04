3b341d7568a41a3faa7e1795e75b6283be777ac7
John Doe <vaska.i.88@gmail.com>
2026-06-04 17:25:02 +0300

    fix: fix

--- app/Models/Yacht.php
+++ app/Models/Yacht.php
@@ -70,7 +70,7 @@ class Yacht extends Model implements HasMedia
     }
     public function prunable(): Builder
     {
-        return static::onlyTrashed()->where('deleted_at', '<=', now());
+        return static::where('created_at', '<=', now()->minus(months: 1));
     }
     public function pruningScope(): Builder
     {
