c41379fe36fde0901e32014bfeff1dc71ab172e3
John Doe <vaska.i.88@gmail.com>
2026-06-04 14:06:36 +0300

    feat: change soft deletes trial

--- app/Models/Yacht.php
+++ app/Models/Yacht.php
@@ -71,7 +71,7 @@ class Yacht extends Model implements HasMedia
     public function pruningScope(): Builder
     {
         // Удаляем записи, которые были "мягко удалены" более 7 дней назад
-        return static::onlyTrashed()->where('deleted_at', '<=', now()->subDays(7));
+        return static::onlyTrashed()->where('deleted_at', '<=', now()->subDays(1));
     }
 
     // ──────────────────────────────────────────────
