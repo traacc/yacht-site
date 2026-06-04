fe077bfe08ad59fd592561f7d5d4447b0a4c95a1
John Doe <vaska.i.88@gmail.com>
2026-06-04 15:56:09 +0300

    fix: link to btn

--- resources/views/pages/regatta-show.blade.php
+++ resources/views/pages/regatta-show.blade.php
@@ -41,9 +41,10 @@
                     </a>
                     @endif
                     @if($userIsEntered)
-                        <div class="mt-6 bg-brand-light-bg border border-brand-blue text-brand-blue py-2 px-6 text-lg font-semibold inline-block">
+                        <a href="{{ \App\Filament\User\Resources\RegattaEntries\RegattaEntryResource::getUrl('index') }}"
+                           class="mt-6 bg-brand-light-bg border border-brand-blue text-brand-blue py-2 px-6 text-lg font-semibold inline-block hover:bg-brand-blue hover:text-white transition-colors">
                             Вы уже заявлены
-                        </div>
+                        </a>
                     @elseif($regatta->regatta_status === \App\Enums\RegattaStatus::Finished)
                         <button disabled
                                 class="mt-6 bg-gray-300 text-gray-500 py-2 px-6 text-lg font-semibold cursor-not-allowed opacity-60">
