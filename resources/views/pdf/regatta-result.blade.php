61e187f4200702e507f26a0f1c5e4782f41177b7
John Doe <vaska.i.88@gmail.com>
2026-06-14 15:54:20 +0300

    fix: pdf regatta results

--- resources/views/pdf/regatta-result.blade.php
+++ resources/views/pdf/regatta-result.blade.php
@@ -174,16 +174,16 @@
             @php($headRowspan = $raceCount > 0 ? 2 : 1)
             <tr>
                 <th rowspan="{{ $headRowspan }}" style="width: 28px;">Место</th>
-                <th rowspan="{{ $headRowspan }}" style="width: 100px;">Парус №</th>
-                <th rowspan="{{ $headRowspan }}" style="width: 150px;">Команда</th>
-                <th rowspan="{{ $headRowspan }}" style="width: 150px;">Яхта</th>
-                <th rowspan="{{ $headRowspan }}" style="width: 250px;">Экипаж</th>
+                <th rowspan="{{ $headRowspan }}" style="width: 40px;">Парус №</th>
+                <th rowspan="{{ $headRowspan }}" style="width: 90px;">Команда</th>
+                <th rowspan="{{ $headRowspan }}" style="width: 80px;">Яхта</th>
+                <th rowspan="{{ $headRowspan }}" style="width: 110px;">Экипаж</th>
                 <th rowspan="{{ $headRowspan }}" style="width: 58px;">Дата рождения</th>
                 <th rowspan="{{ $headRowspan }}" style="width: 42px;">Разряд</th>
                 @for($n = 1; $n <= $raceCount; $n++)
                     <th colspan="2">Гонка {{ $n }}</th>
                 @endfor
-                <th rowspan="{{ $headRowspan }}" style="width: 60px;">Итого очков</th>
+                <th rowspan="{{ $headRowspan }}" style="width: 38px;">Итого очков</th>
             </tr>
             @if($raceCount > 0)
                 <tr>
