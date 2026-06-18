00118fc8e96cca8836142d16ccd5a519ed1c199a
John Doe <vaska.i.88@gmail.com>
2026-06-18 20:33:49 +0300

    feat: can show date in gallery title

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -103,7 +103,7 @@
                          class="absolute w-full h-full object-cover z-10 transition-transform duration-500 group-hover:scale-105">
                     <div class="bg-[#2E325C] opacity-30 absolute z-15 w-full h-full transition-transform duration-500 group-hover:scale-105"></div>
                     <div class="info relative z-20 p-6 pt-56 text-white">
-                        <h4 class="title a-font text-2xl mb-1">{{ $gallery->regatta?->name ?? $gallery->name }}</h4>
+                        <h4 class="title a-font text-2xl mb-1">{{ $gallery->regatta?->name ?? $gallery->name ?? $gallery->date?->isoFormat('D MMMM') }}</h4>
                         @if($gallery->regatta)
                             <p class="text-lg mb-3 opacity-90">{{ $gallery->name }}</p>
                         @endif
