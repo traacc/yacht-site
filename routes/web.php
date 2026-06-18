52b269d2ccb3b9a6a093b92df0f450a2d2065c04
John Doe <vaska.i.88@gmail.com>
2026-06-18 21:41:04 +0300

    fix: show only approved

--- routes/web.php
+++ routes/web.php
@@ -267,7 +267,7 @@ Route::get('/regattas/{regatta}', function (Regatta $regatta) {
 
     $regatta->setRelation('resultItems', $regatta->results->flatMap->items);
 
-    $entries = $regatta->entries;
+    $entries = $regatta->approvedEntries;
 
     $currentWeather = $regatta?->coordinates
         ? app(WeatherService::class)->getWeather(
