6856d0e6fb5c083c37e8712640c17866ee43823a
John Doe <vaska.i.88@gmail.com>
2026-06-22 10:51:29 +0300

    fix: comment out AuthenticateSession middleware in Admin and User panel providers

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -77,7 +77,7 @@ class AdminPanelProvider extends PanelProvider
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
                 StartSession::class,
-                AuthenticateSession::class,
+                //AuthenticateSession::class,
                 ShareErrorsFromSession::class,
                 PreventRequestForgery::class,
                 SubstituteBindings::class,
--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -82,7 +82,7 @@ class UserPanelProvider extends PanelProvider
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
                 StartSession::class,
-                AuthenticateSession::class,
+                //AuthenticateSession::class,
                 ShareErrorsFromSession::class,
                 PreventRequestForgery::class,
                 SubstituteBindings::class,
